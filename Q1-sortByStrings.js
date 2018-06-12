f607a045c2edb16c8fd6fec3e57cdb08bdd96e93
Adam Julier <adamjulier@Kevins-MacBook-Air.local>
2018-06-12 14:21:48 -0400

    q1

--- Q1-sortByStrings.js
+++ Q1-sortByStrings.js
@@ -1,10 +1,26 @@
-function sortByString(s, t) {
-  let chars = s.split("");
-  chars.forEach(char => {
-      t.indexOf(char) > -1
-        ? console.log(char, 'exists')
-        : console.log(char, 'does not exist')
-  })
+function sortByString(s,t) {
+  let newstring = '';
+  let check = [];
+
+  for (let char of s) {
+    !check[char]
+      // If char does not exist already in arr, create a key-value for it
+      ? check[char] = 1
+      // If char already has an existing key-value in the check arr, increment it to account for duplicate letters
+      : check[char]++
+  }
+
+  for (let char of t) {
+    if (check[char]) {
+      // Concat to new string based on number of times the letter appeared
+      newstring += char.repeat(check[char]);
+    }
+  }
+
+  console.log(newstring);
+  return newstring;
 }
 
+
 sortByString('good', 'odg');
+sortByString('weather', 'therapyw');
