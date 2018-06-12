a130a4101b8a826234dfe4e06fa154e9e03f70ea
Adam Julier <adamjulier@Kevins-MacBook-Air.local>
2018-06-12 16:16:13 -0400

    coefficient parsing

--- Q2-decodeString.js
+++ Q2-decodeString.js
@@ -1,5 +1,10 @@
 let encodedstr = '5[testing123]';
 
+let coefficient = encodedstr.match(/^[^\[}),]*/);
+
 let parsed = encodedstr.match(/\[(.*?)\]/);
+
+
+console.log(coefficient[0]);
 
 console.log(parsed[1]);
