d1629e6171e48d06e49b0ba1946f5db6f32d336f
Adam Julier <adamjulier@Kevins-MacBook-Air.local>
2018-06-12 17:24:20 -0400

    q2

--- Q2-decodeString.js
+++ Q2-decodeString.js
@@ -3,32 +3,39 @@ function decodeString(s) {
   let coefficient = s.match(/^[^\[,]*/);
   let parsed = s.match(/\[(.*?)\]/);
 
-  console.log('coefficient: ', coefficient[0]);
-  console.log('parsed str: ', parsed[1]);
+    console.log('coefficient: ', coefficient[0]);
+    console.log('parsed str: ', parsed[1]);
 
-  // Get nested string (if exists)
-  let innerstr = s.match(/\[[^[\]]*\]/);
-  let innerparsed = innerstr[0].match(/\[(.*?)\]/);
-  console.log('inner parsed: ', innerparsed[1]);
-  // Get nested coefficient (if exists -- will be used as the nested check);
+  // Get nested coefficient (if exists -- * this will be used as the nested check);
   let innercoefficient = parsed[1].replace(/\D+/g, '');
-  console.log('inner coefficient: ', innercoefficient[0])
-  // let outerparsed = parsed[1].replace(/\d+/g, '');
-  let outerparsed = parsed[1].match(/.+?(?=[0-9])/);
-  console.log('outer parsed: ', outerparsed[0]);
+  innercoefficient
+  ? console.log('inner coefficient exists: ', innercoefficient[0])
+  : console.log('nested string does not exist')
+
 
 
   if (!innercoefficient) {
-    // No nested string exists
+    // No nested string exists; decode normally
     decodedstr = parsed[1].repeat(coefficient);
   } else {
-    // Nested string exists
+    // Nested string exists; combine the inner parts together, and then decode
 
+    // Get nested string (if exists)
+    let innerstr = s.match(/\[[^[\]]*\]/);
+    let innerparsed = innerstr[0].match(/\[(.*?)\]/);
+      console.log('inner parsed: ', innerparsed[1]);
+    // Get first outer string (if nested exists)
+    let outerparsed = parsed[1].match(/.+?(?=[0-9])/);
+      console.log('outer parsed: ', outerparsed[0]);
+
+    innerdecoded = innerparsed[1].repeat(innercoefficient);
+    innerconcat = outerparsed[0] + innerdecoded;
+    decodedstr = innerconcat.repeat(coefficient);
   }
 
-  console.log('decoded str: ', decodedstr);
+  console.log('** Decoded string ** : ', decodedstr);
   return decodedstr;
 }
 
-// decodeString('4[ab]');
+decodeString('4[ab]');
 decodeString('2[b3[a]]');
