41665582e1a7734bd1039895703663fcda2b4d05
Adam Julier <adamjulier@Kevins-MacBook-Air.local>
2018-06-12 19:13:14 -0400

    subdenom start

--- Q3-changePossibilities.js
+++ Q3-changePossibilities.js
@@ -2,20 +2,26 @@ function changePossibilities(amount, denominations) {
     combos = [];
 
     denominations.forEach(denom => {
-      // get #'s complement, check if it's in arr
+      // get #'s complement
       let complement = amount - denom;
+      // check if complement exists in denominations arr
       if (denominations.indexOf(complement) > -1) {
-        // create combo as sorted values (to avoid duplicates), into a string (for indexOf)
-        let combo = [denom, complement].sort().toString();
-        // if combo isn't already in the combos array, add it
-        if (combos.indexOf(combo) === -1) {
-          combos.push(combo);
+          // create combo as values, sorted (to avoid duplicates), into a string (for indexOf check to work)
+          let combo = [denom, complement].sort().toString();
+          // if combo isn't already in the combos array, add it
+          if (combos.indexOf(combo) === -1) {
+            combos.push(combo);
+          }
         }
-      }
+      // check if any other denominations equal that complement #
+      denominations.forEach(subdenom => {
+        
+      })
 
     })
 
     console.log('Combos: ', combos);
+    console.log('# of combos: ', combos.length)
 }
 
 changePossibilities(4, [1,2,3]);
