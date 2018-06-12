ce740ac7451715565d45927c7427e09730bf5aa0
Adam Julier <adamjulier@Kevins-MacBook-Air.local>
2018-06-12 16:54:49 -0400

    nested coefficient

--- Q2-decodeString.js
+++ Q2-decodeString.js
@@ -3,14 +3,27 @@ function decodeString(s) {
   let parsed = s.match(/\[(.*?)\]/);
 
   let decodedstr = parsed[1].repeat(coefficient);
-
   // console.log(decodedstr);
+
   console.log('coefficient: ', coefficient[0]);
   console.log('parsed str: ', parsed[1]);
 
-  let inner = s.match(/\[[^[\]]*\]/);
-  let innerparsed = inner[0].match(/\[(.*?)\]/);
-  console.log('inner parsed: ', innerparsed[1])
+  // Get nested string (if exists)
+  let innerstr = s.match(/\[[^[\]]*\]/);
+  let innerparsed = innerstr[0].match(/\[(.*?)\]/);
+  console.log('inner parsed: ', innerparsed[1]);
+  // Get nested coefficient (if exists);
+  let innercoefficient = parsed[1].replace(/\D+/g, '');
+  console.log('inner coefficient: ', innercoefficient[0])
+
+
+  if (!innercoefficient) {
+    // No nested str
+    console.log('no nested str');
+  } else {
+    // Nested str detected
+    console.log('nested str detected');
+  }
 
   return decodedstr;
 }
